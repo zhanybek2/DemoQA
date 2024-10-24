25f1da4ea41ecec7b4c83289d59b52e52e674bf7
Zhanybek2 <zhanybekTalip2@gmail.com>
2024-10-25 00:43:25 +0600

    бновление конфигурации и драйвера Chrome, удаление geckodriver

--- src/main/java/demoqa/drivers/ChromeWebDriver.java
+++ src/main/java/demoqa/drivers/ChromeWebDriver.java
@@ -7,7 +7,13 @@ import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
+
+import static demoqa.utils.ConfigReader.getValue;
 
 public class ChromeWebDriver {
     public static WebDriver loadChromeDriver(){
@@ -17,19 +23,18 @@ public class ChromeWebDriver {
         options.addArguments("--disable-extensions");
         options.addArguments("--window-size=1920,1080");
         options.addArguments("--no-sandbox");
-
-
-
-        if(Boolean.parseBoolean(ConfigReader.getValue("headless"))){
+        if (Boolean.parseBoolean(getValue("headless"))) {
             options.addArguments("--headless");
         }
-
-//        Cookie cookie = new Cookie("JSESSIONID",ConfigReader.getValue("cookie"));
-
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
+        // Specify the URL of the remote WebDriver server
+        WebDriver driver = null;
+        try {
+            driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), options);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-//        driver.manage().addCookie(cookie);
+        driver.manage().window().maximize();
         return driver;
     }
 }
